673d085e52dd99b1804fca56d1b1f7043ffbf82b
Vann-Dogg <vann.dogg.93@gmail.com>
2020-05-31 21:19:53 +0300

    SettingIcon

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 
 const Header = ({ data }) => (
   <header id="header">
-    <div className="back-button-block"></div>
+    <div className="back-block"></div>
     <div className="logo-block">
       <img src="//listenpraythink.com/wp-content/uploads/2017/09/logo.png" alt="logo" width="100" />
     </div>
-    <div className="account-icon-block"></div>
+    <div className="setting-block">
+      <FontAwesomeIcon icon={ faCog } />
+    </div>
   </header>
 )
 
